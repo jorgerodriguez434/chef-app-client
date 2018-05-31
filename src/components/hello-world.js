482b358d4146f97751946f1d79fa234c96a8ebbe
Jorge R <jorgerodriguez434@gmail.com>
2018-05-30 23:06:45 -0500

    first commit

---
+++ src/components/hello-world.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { render } from "react-dom";
+
+export default class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1> hello world </h1>
+      </div>
+    );
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HelloWorld from "./components/hello-world"
+
+ReactDOM.render(<HelloWorld />, document.getElementById('root'));
