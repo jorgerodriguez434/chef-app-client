8e3465c5ed59015c0ce99372bb2cb33a47d2b303
Jorge R <jorgerodriguez434@gmail.com>
2018-05-31 09:40:11 -0500

    add landing, home links

--- src/index.js
+++ src/index.js
@@ -6,17 +6,22 @@ import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import LoginForm from "./components/login-form";
 import Dashboard from "./components/dashboard";
 import Menu from "./components/menu";
+import Home from "./components/home";
+import Landing from "./components/landing";
 
 import "./index.css";
 const App = () => (
   <BrowserRouter>
     <Provider store={store}>
       <div>
-        <Link to="/">Home</Link>
+        <Link to="/login">Login</Link>
+        <Link to="/home">Home</Link>
         <Link to="/dashboard">Dashboard</Link>
         <Link to="/menu">Menu</Link>
         <Switch>
-          <Route exact path="/" component={LoginForm} />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/login" component={LoginForm} />
+          <Route exact path="/home" component={Home} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/menu" component={Menu} />
         </Switch>
