1437d29dc10657f0f197a2fb2c7868001987d188
Jorge R <jorgerodriguez434@gmail.com>
2018-06-05 15:14:24 -0500

    add style to buttons

--- src/components/button-group.js
+++ src/components/button-group.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const ButtonGroup = (props) => (
   <div>
-    <button onClick={props.method1}> filter button </button>
-    <button onClick={props.method2}> no meat button </button>
-    <button onClick={props.method3}> meat button </button>
-    <button onClick={props.method4}> no gluten button </button>
+    <button className="button" onClick={props.method1}> filter button </button>
+    <button className="button" onClick={props.method2}> no meat button </button>
+    <button className="button" onClick={props.method3}> meat button </button>
+    <button className="button" onClick={props.method4}> no gluten button </button>
 </div>
 );
 
