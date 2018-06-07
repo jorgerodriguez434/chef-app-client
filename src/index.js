3a219c5963ae3c47e343e3a17520bbd9582d8aa6
Jorge R <jorgerodriguez434@gmail.com>
2018-06-07 10:47:12 -0500

    update nav style

--- src/index.js
+++ src/index.js
@@ -8,25 +8,24 @@ import Dashboard from "./components/dashboard";
 import Menu from "./components/menu";
 import Home from "./components/home";
 import Seating from "./components/seating";
-import Testing from "./components/testing";
 
 import "./index.css";
 const App = () => (
   <BrowserRouter>
     <Provider store={store}>
       <div>
-        <Link to="/home">Home</Link>
-        <Link to="/dashboard">Dashboard</Link>
-        <Link to="/menu">Menu</Link>
-        <Link to="/seating">Seating</Link>
-        <Link to="/testing">Testing</Link>
+      <section className="link-group">
+        <Link className="link "to="/home">Home</Link>
+        <Link className="link" to="/dashboard">Dashboard</Link>
+        <Link className="link" to="/menu">Menu</Link>
+        <Link className="link" to="/seating">Seating</Link>
+      </section>
         <Switch>
           <Route exact path="/" component={LoginForm} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/dashboard" component={Dashboard} />
           <Route exact path="/menu" component={Menu} />
           <Route exact path="/seating" component={Seating} />
-          <Route exact path="/testing" component={Testing} />
         </Switch>
       </div>
     </Provider>
