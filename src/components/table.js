91aaadbdd1fce849b295ac051ef0885053b06621
Jorge R <jorgerodriguez434@gmail.com>
2018-06-07 12:47:31 -0500

    add reservation feature

--- src/components/table.js
+++ src/components/table.js
@@ -3,26 +3,65 @@ import React from "react";
 export default class Table extends React.Component {
   constructor() {
     super();
-    this.state = { reserved: false, classColor: "" };
+    this.state = { reserved: false, buttonName: "Reserve" };
   }
-  changeColor = () => {
+  handleReserve = () => {
     console.log(`table ${this.props.number} has been clicked!`);
     //change color
     this.setState({
-      classColor: "red"
+      reserved: true,
+      classColor: "red",
+      buttonName: "cancel"
+    });
+  };
+  handleCancel = () => {
+    console.log(`table ${this.props.number} has been clicked!`);
+    //change color
+    this.setState({
+      reserved: false,
+      classColor: "white",
+      buttonName: "reserve"
     });
   };
 
   render() {
-    return (
-      <div className="table-row">
-        <h2> This is a table </h2>
-        <div className={`table ${this.state.classColor}`} onClick={this.changeColor}>
-          <p>Table </p>
-          <p> {this.props.number} </p>
+    if (this.state.reserved === false) {
+      return (
+        <div className="table-row">
+          <h2> This is a table </h2>
+          <div className={`table ${this.state.classColor}`} onClick={this.handleReserve}>
+            <p>Table </p>
+            <p> {this.props.number} </p>
+          </div>
+          <button className="button" onClick={this.handleReserve}>
+            {" "}
+            {this.state.buttonName}{" "}
+          </button>
         </div>
-        <button className="button"> Reserve </button>
-      </div>
-    );
+      );
+    }//if
+    else if (this.state.reserved === true) {
+      return (
+        <div className="table-row">
+          <h2> This is a table </h2>
+          <div className={`table ${this.state.classColor}`} onClick={this.handleCancel}>
+            <p>Table </p>
+            <p> {this.props.number} </p>
+          </div>
+          <button className="button" onClick={this.handleCancel}>
+            {" "}
+            {this.state.buttonName}{" "}
+          </button>
+          <h5> How many guests will be accompanying you? </h5>
+          <input className="how-many-guests-input my-text"/>
+        </div>
+      );
+    }//if
   }
 }
+
+/*
+
+
+
+*/
