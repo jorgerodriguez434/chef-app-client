db702eab5feca2f66c3195debfa3a9552b3a8ee0
Jorge R <jorgerodriguez434@gmail.com>
2018-07-09 11:22:37 -0500

    add heroku urls

--- src/config.js
+++ src/config.js
@@ -1,10 +1,14 @@
 exports.API_BASE_URL =  process.env.REACT_APP_API_BASE_URL ||
-"http://localhost:8080/api/dishes"
+//"http://localhost:8080/api/dishes"
+"https://thawing-ravine-79238.herokuapp.com/api/dishes"
 exports.API_USERS_URL =  process.env.API_USERS_URL ||
-"http://localhost:8080/api/users"
+//"http://localhost:8080/api/users"
+"https://thawing-ravine-79238.herokuapp.com/api/users"
 exports.API_AUTH_LOGIN_URL =  process.env.API_AUTH_LOGIN_URL ||
-"http://localhost:8080/api/auth/login"
+//"http://localhost:8080/api/auth/login"
+"https://thawing-ravine-79238.herokuapp.com/api/auth/login"
 exports.API_PROTECTED_URL =  process.env.API_PROTECTED_URL||
-"http://localhost:8080/api/protected"
+//"http://localhost:8080/api/protected"
+"https://thawing-ravine-79238.herokuapp.com/api/protected"
 
   
