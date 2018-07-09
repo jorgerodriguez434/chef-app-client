589398f2bd60b5a7574c380e18fbd6f2d37ff490
Jorge R <jorgerodriguez434@gmail.com>
2018-07-09 13:41:02 -0500

    add spinner

--- src/components/home.js
+++ src/components/home.js
@@ -4,7 +4,6 @@ import LoginForm from "./login-form"
 import PostDish from "./post-dish"
 import HomeContent from "./home-content"
 import { connect } from "react-redux";
-import * as actions from "../actions";
 import "./home.css";
 
 export class Home extends React.Component {
@@ -17,58 +16,51 @@ export class Home extends React.Component {
 
   getStarted = () => {
     console.log("get started button clicked!");
-    /*this.setState({
+    this.setState({
       display: "get started"
-    }); */
-    this.props.dispatch(actions.setDisplay("get started"));
+    });
   };
   getStartedTwo = () => {
     console.log("get started button clicked!");
-    /*this.setState({
+    this.setState({
       display: "post dish"
-    }); */
-    this.props.dispatch(actions.setDisplay("post dish"));
+    });
   };
 
   componentDidMount = () => {
       if (localStorage.getItem("isAuthenticated")){
-        /*this.setState({
+        this.setState({
             display: "secured landing"
-        }); */
-        this.props.dispatch(actions.setDisplay("secured landing"));
+        });
       }
   } 
  
   setSecuredLanding = () => {
-      /*this.setState({
+      this.setState({
           display: "secured landing"
-      });*/
-      this.props.dispatch(actions.setDisplay("secured landing"));
+      });
   }
 //onClick={this.getStarted}
   render = () => {
-    console.log(this.props);
-    //console.log(this.state);
-    if (this.props.state.display === "landing") {
+    console.log(this.props.state);
+    console.log(this.state);
+    if (this.state.display === "landing") {
       return (
         <HomeContent onClick={this.getStarted}/>
       );
     }
-    if (this.props.state.display  === "get started") {
+    if (this.state.display === "get started") {
       return <LoginForm />;
     }
-     if (this.props.state.display  === "secured landing"){
+     if (this.state.display === "secured landing"){
       return(
       <div>
       <Links/>
       <HomeContent onClick={this.getStartedTwo}/>
       </div>)
     }//if
-    if (this.props.state.display  === "post dish") {
+    if (this.state.display === "post dish") {
       return <PostDish />;
-    }
-    if (this.props.state.display === "login") {
-      return <LoginForm />;
     }
   };
 }
--- src/components/login-form.js
+++ src/components/login-form.js
@@ -140,4 +140,4 @@ export const mapStateToProps = state => ({
   state
 });
 
-export default connect(mapStateToProps)(LoginForm)
+export default connect(mapStateToProps)(LoginForm);
