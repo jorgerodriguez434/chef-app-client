37fab459cb54290012f2d3bf259ad9de6d3bd459
Jorge R <jorgerodriguez434@gmail.com>
2018-07-30 23:49:26 -0500

    clean links

--- src/components/links.js
+++ src/components/links.js
@@ -2,47 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-import "./links.css"
-import { Redirect } from 'react-router-dom';
+import "./links.css";
+import { Redirect } from "react-router-dom";
 
 export class Links extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: "Log out",
+      display: "landing",
+      isPending: false
+    };
+  }
 
-    constructor(){
-        super();
-        this.state = {
-            name: "Log out",
-            display: "landing",
-            isPending: false
-        }
-    }
-
-    
   logout = () => {
     console.log("logout button clicked!");
     localStorage.removeItem("token");
     localStorage.removeItem("isAuthenticated");
     this.setState({
-        name: "Logged out!",
-        display: "login",
-        isPending: false
+      name: "Logged out!",
+      display: "login",
+      isPending: false
     });
     this.props.dispatch(actions.setLogOut());
   };
 
   wait = () => {
     this.setState({
-        name: "Logging out...",
-        isPending: true
+      name: "Logging out...",
+      isPending: true
     });
     setTimeout(this.logout, 1000);
-  } 
+  };
 
   onClick = () => {
-      console.log("clicked!");
-      this.props.dispatch(actions.setDisplay("landing"));
-  }
+    console.log("clicked!");
+    this.props.dispatch(actions.setDisplay("landing"));
+  };
   render() {
-  if (this.state.display === "landing"){
+    if (this.state.display === "landing") {
       return (
         <div className="outside-link-group-container">
           <section className="link-group">
@@ -55,27 +53,26 @@ export class Links extends React.Component {
             <Link className="link" to="/post-dish" onClick={this.onClick}>
               Post Dish
             </Link>
-          
 
-       <div className="logout-div">
-            <button className="logout" onClick={this.wait}>
-              {" "}
-              {this.state.name}
-            </button>
-        </div>
+            <div className="logout-div">
+              <button className="logout" onClick={this.wait}>
+                {" "}
+                {this.state.name}
+              </button>
+            </div>
           </section>
         </div>
       );
     }
 
-    if (this.state.display === "login"){
-      return <Redirect to='/login'/>
+    if (this.state.display === "login") {
+      return <Redirect to="/login" />;
     }
   }
 }
 
 export const mapStateToProps = state => ({
-    state
-  });
-  
-  export default connect(mapStateToProps)(Links);
+  state
+});
+
+export default connect(mapStateToProps)(Links);
